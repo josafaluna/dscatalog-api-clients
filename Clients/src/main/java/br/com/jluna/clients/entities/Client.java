8617d271f1394c777d18a7d1647a0abd8055236e
Josafá Luna <josafaluna@gmail.com>
2021-03-23 08:59:57 -0300

    access jpa, h2

--- Clients/src/main/java/br/com/jluna/clients/entities/Client.java
+++ Clients/src/main/java/br/com/jluna/clients/entities/Client.java
@@ -3,15 +3,31 @@ package br.com.jluna.clients.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
+
 	private Integer children;
 
 	public Client() {
@@ -24,6 +40,16 @@ public class Client implements Serializable {
 		this.income = income;
 		this.birthDate = birthDate;
 		this.children = children;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		this.birthDate = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		this.birthDate = Instant.now();
 	}
 
 	public Long getId() {
