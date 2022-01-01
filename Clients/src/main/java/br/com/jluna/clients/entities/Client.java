37a11a11e0dc15c2f65200e8bb816820db53f4f6
Josafá Luna <josafaluna@gmail.com>
2022-01-01 18:18:22 -0300

    Algumas melhorias no código final

--- Clients/src/main/java/br/com/jluna/clients/entities/Client.java
+++ Clients/src/main/java/br/com/jluna/clients/entities/Client.java
@@ -1,128 +1,108 @@
 package br.com.jluna.clients.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_client")
 public class Client implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private String cpf;
-	private Double income;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
 
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant birthDate;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant birthDate;
 
-	private Integer children;
+    private Integer children;
 
-	public Client() {
-	}
+    public Client() {
+    }
 
-	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
-		this.id = id;
-		this.name = name;
-		this.cpf = cpf;
-		this.income = income;
-		this.birthDate = birthDate;
-		this.children = children;
-	}
+    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
 
-	@PrePersist
-	public void prePersist() {
-		this.birthDate = Instant.now();
-	}
+    @PrePersist
+    public void prePersist() {
+        this.birthDate = Instant.now();
+    }
 
-	@PreUpdate
-	public void preUpdate() {
-		this.birthDate = Instant.now();
-	}
+    @PreUpdate
+    public void preUpdate() {
+        this.birthDate = Instant.now();
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	public Double getIncome() {
-		return income;
-	}
+    public Double getIncome() {
+        return income;
+    }
 
-	public void setIncome(Double income) {
-		this.income = income;
-	}
+    public void setIncome(Double income) {
+        this.income = income;
+    }
 
-	public Instant getBirthDate() {
-		return birthDate;
-	}
+    public Instant getBirthDate() {
+        return birthDate;
+    }
 
-	public void setBirthDate(Instant birthDate) {
-		this.birthDate = birthDate;
-	}
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
 
-	public Integer getChildren() {
-		return children;
-	}
+    public Integer getChildren() {
+        return children;
+    }
 
-	public void setChildren(Integer children) {
-		this.children = children;
-	}
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Client other = (Client) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
 }
--- Clients/src/main/java/br/com/jluna/clients/services/ClientService.java
+++ Clients/src/main/java/br/com/jluna/clients/services/ClientService.java
@@ -1,91 +1,92 @@
 package br.com.jluna.clients.services;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.jluna.clients.dto.ClientDTO;
+import br.com.jluna.clients.entities.Client;
+import br.com.jluna.clients.repository.ClientRepository;
+import br.com.jluna.clients.services.exceptions.ResourceNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import br.com.jluna.clients.dto.ClientDTO;
-import br.com.jluna.clients.entities.Client;
-import br.com.jluna.clients.repository.ClientRepository;
-import br.com.jluna.clients.services.exceptions.ResourceNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
 
-	@Autowired
-	private ClientRepository repository;
+    private final ClientRepository repository;
 
-	// paginação
-	@Transactional(readOnly = true)
-	public Page<ClientDTO> findAllPagened(PageRequest pageRequest) {
-		Page<Client> list = repository.findAll(pageRequest);
-		return list.map(c -> new ClientDTO(c));
-	}
+    public ClientService(ClientRepository repository) {
+        this.repository = repository;
+    }
 
-	@Transactional(readOnly = true)
-	public List<ClientDTO> listar() {
-		List<Client> list = repository.findAll();
-		return list.stream().map(c -> new ClientDTO(c)).collect(Collectors.toList());
-	}
+    // paginação
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAllPagened(PageRequest pageRequest) {
+        Page<Client> list = repository.findAll(pageRequest);
+        return list.map(ClientDTO::new);
+    }
 
-	// por ID
-	@Transactional(readOnly = true)
-	public ClientDTO findById(Long id) {
-		Optional<Client> opt = repository.findById(id);
-		Client cli = opt.orElseThrow(() -> new ResourceNotFoundException("ID Not Found: " + id));
-		return new ClientDTO(cli);
-	}
+    @Transactional(readOnly = true)
+    public List<ClientDTO> listar() {
+        List<Client> list = repository.findAll();
+        return list.stream().map(ClientDTO::new).collect(Collectors.toList());
+    }
 
-	// novo
-	@Transactional
-	public ClientDTO insert(@RequestBody ClientDTO dto) {
-		var client = new Client();
-		copyToEntity(dto, client);
-		client = repository.save(client);
+    // por ID
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> opt = repository.findById(id);
+        Client cli = opt.orElseThrow(() -> new ResourceNotFoundException("ID Not Found: " + id));
+        return new ClientDTO(cli);
+    }
 
-		return new ClientDTO(client);
-	}
+    // novo
+    @Transactional
+    public ClientDTO insert(@RequestBody ClientDTO dto) {
+        var client = new Client();
+        copyToEntity(dto, client);
+        client = repository.save(client);
 
-	@Transactional
-	public ClientDTO update(Long id, ClientDTO dto) {
+        return new ClientDTO(client);
+    }
 
-		try {
-			var client = repository.getOne(id);
-			copyToEntity(dto, client);
-			client = repository.save(client);
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
 
-			return new ClientDTO(client);
+        try {
+            var client = repository.getOne(id);
+            copyToEntity(dto, client);
+            client = repository.save(client);
 
-		} catch (Exception e) {
-			throw new ResourceNotFoundException("ID not found " + id);
-		}
+            return new ClientDTO(client);
 
-	}
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("ID not found " + id);
+        }
 
-	@Transactional
-	public void delete(Long id) {
-		try {
-			repository.deleteById(id);
+    }
 
-		} catch (Exception e) {
-			throw new ResourceNotFoundException("ID not found " + id);
-		}
+    @Transactional
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
 
-	}
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("ID not found " + id);
+        }
 
-	private void copyToEntity(ClientDTO dto, Client client) {
-		client.setBirthDate(dto.getBirthDate());
-		client.setChildren(dto.getChildren());
-		client.setCpf(dto.getCpf());
-		client.setIncome(dto.getIncome());
-		client.setName(dto.getName());
-	}
+    }
+
+    private void copyToEntity(ClientDTO dto, Client client) {
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setName(dto.getName());
+    }
 
 }
